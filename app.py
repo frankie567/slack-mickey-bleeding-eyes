e0e79b44a4891f347aa73e04ce67d273d66176b0
François Voron <fvoron@wuha.io>
2018-11-28 17:26:32 +0100

    Fix signature verification

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ SLACK_SIGNING_SECRET = os.environ['SLACK_SIGNING_SECRET']
 
 app = Flask(__name__)
 
-def verify_signature(timestamp, signature):
-    req = str.encode('v0:' + str(timestamp) + ':') + request.data
+def verify_signature(timestamp, signature, data):
+    req = str.encode('v0:' + str(timestamp) + ':') + data
     request_hash = 'v0=' + hmac.new(
         str.encode(SLACK_SIGNING_SECRET),
         req, hashlib.sha256
@@ -25,7 +25,7 @@ def mickey_bleeding_eyes_command():
     req_timestamp = request.headers.get('X-Slack-Request-Timestamp')
     req_signature = request.headers.get('X-Slack-Signature')
     app.logger.debug('Request: X-Slack-Request-Timestamp=%s X-Slack-Signature=%s', req_timestamp, req_signature)
-    if req_timestamp is None or req_signature is None or not verify_signature(req_timestamp, req_signature):
+    if req_timestamp is None or req_signature is None or not verify_signature(req_timestamp, req_signature, request.data):
         return abort(401)
 
     return 'https://media1.tenor.com/images/1f23440dadb7f1d30749d0f41706bd48/tenor.gif?itemid=9801173', 200
