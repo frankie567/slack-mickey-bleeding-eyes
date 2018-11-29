7595cd37fb65a8af224ed3d494613d4005c81a95
François Voron <fvoron@wuha.io>
2018-11-29 09:09:59 +0100

    Fix Slack message and serve image

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import abort, Flask, jsonify, request
+from flask import abort, Flask, jsonify, request, send_from_directory
 import hashlib
 import hmac
 import logging
@@ -6,7 +6,7 @@ import os
 
 SLACK_SIGNING_SECRET = os.environ['SLACK_SIGNING_SECRET']
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 def verify_signature(timestamp, signature, data):
     req = str.encode('v0:' + str(timestamp) + ':') + data
@@ -25,13 +25,23 @@ def mickey_bleeding_eyes_command():
     req_timestamp = request.headers.get('X-Slack-Request-Timestamp')
     req_signature = request.headers.get('X-Slack-Signature')
     app.logger.debug('Request: X-Slack-Request-Timestamp=%s X-Slack-Signature=%s', req_timestamp, req_signature)
-    #if req_timestamp is None or req_signature is None or not verify_signature(req_timestamp, req_signature, request.get_data()):
-        #return abort(401)
+    if req_timestamp is None or req_signature is None or not verify_signature(req_timestamp, req_signature, request.get_data()):
+        return abort(401)
 
-    return jsonify(
-        text='https://i.pinimg.com/originals/ab/c5/02/abc50276a99daa149a51119912de6c70.png',
-        as_user=True
-    ), 200
+    response = {
+        'text': 'https://slack-mickey-bleeding-eyes.herokuapp.com/image',
+        'attachments': [
+            {
+                'image_url': 'https://slack-mickey-bleeding-eyes.herokuapp.com/image'
+            }
+        ]
+    }
+
+    return jsonify(response), 200
+
+@app.route('/image')
+def image():
+    return send_from_directory('', 'image.png')
 
 if __name__ != '__main__':
     gunicorn_logger = logging.getLogger('gunicorn.error')
